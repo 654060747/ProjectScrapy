75e562f956f7ef89ca31dd12879263330e658996
654060747 <654060747@qq.com>
2021-05-24 15:20:30 +0800

    手动声明日志模块，或者setting设置，二者选其一

--- scrapys/scrapys/log_/__init__.py
+++ scrapys/scrapys/log_/__init__.py
@@ -1,10 +1,10 @@
 import logging
 from logging import Formatter, FileHandler
 
-logger = logging.getLogger('liuliu')
+logger = logging.getLogger('scrapys')
 logger.setLevel(logging.INFO)
 
-handler = FileHandler('./liuliu.log')
+handler = FileHandler('./xx.log')
 handler.setLevel(logging.INFO)
 handler.setFormatter(Formatter(fmt='%(asctime)s: %(message)s',datefmt='%Y-%m-%d %H-%M-%S'))
 
